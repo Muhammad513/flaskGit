a803f1839c26115140a40c8b34063341cce5572f
Muhammad513 <chinobod.chinobod@bk.ru>
2021-03-26 16:51:41 +0500

    new homework exchangerapi

---
+++ lesson4/main.py
@@ -0,0 +1,8 @@
+import requests
+date="2015-02-25"
+url=f'https://api.exchangeratesapi.io/{date}'
+payloads={
+    "base":"USD",
+    "symbols":["EUR","GBP","CAD","TRY",]}
+r=requests.get(url,params=payloads).json()
+print
